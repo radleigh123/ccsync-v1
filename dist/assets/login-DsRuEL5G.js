cdb3bc6fb1ce2be22c94785845c8fe27a68f130b
Radleigh123 <103168649+radleigh123@users.noreply.github.com>
2025-08-24 19:23:21 +0800

    Updates

---
+++ dist/assets/login-DsRuEL5G.js
@@ -0,0 +1,1 @@
+import"./main-B_59QPUW.js";(()=>{const e=document.querySelectorAll(".needs-validation");Array.from(e).forEach(a=>{a.addEventListener("submit",t=>{a.checkValidity()||(t.preventDefault(),t.stopPropagation()),a.classList.add("was-validated")},!1)})})();
