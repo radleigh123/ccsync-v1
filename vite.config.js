bc9a1d6f184bf791cc3a038fdfb7cac65921447f
Radleigh123 <103168649+radleigh123@users.noreply.github.com>
2025-12-16 10:38:37 +0800

    UPDATE Added pages to vite

--- vite.config.js
+++ vite.config.js
@@ -10,27 +10,50 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 main: resolve(__dirname, 'src', 'index.html'),
-                login: resolve(__dirname, 'src/pages/auth/login.html'),
+                designSystem: resolve(__dirname, 'src', 'design-system.html'),
+
+                // ADMIN
+                adminIndex: resolve(__dirname, 'src', 'pages', 'admin', 'index.html'),
+                adminEditUser: resolve(__dirname, 'src', 'pages', 'admin', 'edit-user.html'),
+
+                // AUTH
+                login: resolve(__dirname, 'src', 'pages', 'auth', 'login.html'),
                 register: resolve(__dirname, 'src', 'pages', 'auth', 'register.html'),
                 forgotPassword: resolve(__dirname, 'src', 'pages', 'auth', 'forgot-password.html'),
-                profile: resolve(__dirname, 'src', 'pages', 'profile', 'profile.html'),
+
+                // HOME
                 home: resolve(__dirname, 'src', 'pages', 'home', 'home.html'),
+
+                homeEventPersonAdd: resolve(__dirname, 'src', 'pages', 'home', 'event', 'add-event-person.html'),
+                homeEventAdd: resolve(__dirname, 'src', 'pages', 'home', 'event', 'add-event.html'),
+                homeEventEdit: resolve(__dirname, 'src', 'pages', 'home', 'event', 'edit-event.html'),
+                homeEventSingleView: resolve(__dirname, 'src', 'pages', 'home', 'event', 'view-event-single.html'),
+                homeEventView: resolve(__dirname, 'src', 'pages', 'home', 'event', 'view-event.html'),
+
                 homeMemberAdd: resolve(__dirname, 'src', 'pages', 'home', 'member', 'register-member.html'),
                 homeMemberView: resolve(__dirname, 'src', 'pages', 'home', 'member', 'view-member.html'),
-                homeEventAdd: resolve(__dirname, 'src', 'pages', 'home', 'event', 'add-event.html'),
-                homeEventView: resolve(__dirname, 'src', 'pages', 'home', 'event', 'view-event.html'),
-                homeEventEdit: resolve(__dirname, 'src', 'pages', 'home', 'event', 'edit-event.html'),
-                homeEventPersonAdd: resolve(__dirname, 'src', 'pages', 'home', 'event', 'add-event-person.html'),
-                homeEventSingleView: resolve(__dirname, 'src', 'pages', 'home', 'event', 'view-event-single.html'),
-                homeRequirementAdd: resolve(__dirname, 'src', 'pages', 'home', 'requirement', 'add-requirement.html'),
-                homeRequirementView: resolve(__dirname, 'src', 'pages', 'home', 'requirement', 'view-requirement.html'),
+
                 homeOfficerAdd: resolve(__dirname, 'src', 'pages', 'home', 'officer', 'add-officer.html'),
+                homeOfficerEdit: resolve(__dirname, 'src', 'pages', 'home', 'officer', 'edit-officer.html'),
+                homeOfficerViewDetails: resolve(__dirname, 'src', 'pages', 'home', 'officer', 'view-officer-details.html'),
                 homeOfficerView: resolve(__dirname, 'src', 'pages', 'home', 'officer', 'view-officer.html'),
-                settings: resolve(__dirname, 'src', 'pages', 'settings', 'settings.html'),
-                designSystem: resolve(__dirname, 'src', 'design-system.html'),
-                homeEventViewStudent: resolve(__dirname, 'src', 'pages', 'home', 'student', 'student-dashboard.html'),
-                homeOfficerViewStudent: resolve(__dirname, 'src', 'pages', 'home', 'student', 'studentViewOfficer.html'),
-                homeProfileViewStudent: resolve(__dirname, 'src', 'pages', 'home', 'student', 'studentProfile.html'),
+
+                homeRequirementAddPerson: resolve(__dirname, 'src', 'pages', 'home', 'requirement', 'add-requirement-person.html'),
+                homeRequirementAdd: resolve(__dirname, 'src', 'pages', 'home', 'requirement', 'add-requirement.html'),
+                homeRequirementViewDetail: resolve(__dirname, 'src', 'pages', 'home', 'requirement', 'view-detail-requirement.html'),
+                homeRequirementViewSingle: resolve(__dirname, 'src', 'pages', 'home', 'requirement', 'view-requirement-single.html'),
+                homeRequirementView: resolve(__dirname, 'src', 'pages', 'home', 'requirement', 'view-requirement.html'),
+
+                homeStudentDashboard: resolve(__dirname, 'src', 'pages', 'home', 'student', 'student-dashboard.html'),
+                homeStudentFullView: resolve(__dirname, 'src', 'pages', 'home', 'student', 'studentFullView.html'),
+                homeStudentProfile: resolve(__dirname, 'src', 'pages', 'home', 'student', 'studentProfile.html'),
+                homeStudentOfficer: resolve(__dirname, 'src', 'pages', 'home', 'student', 'studentViewOfficer.html'),
+
+                // PROFILE
+                profile: resolve(__dirname, 'src', 'pages', 'profile', 'profile.html'),
+
+                // SETTINGS
+                settings: resolve(__dirname, 'src', 'pages', 'settings', 'settings.html')
             }
         }
     },
